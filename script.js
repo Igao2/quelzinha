52c81cc0d66e7f0ebcdad6507bfe9c1420d5d0c9
Igao2 <igor.dasilvacosta8@gmail.com>
2024-06-12 15:00:38 -0300

    Teamo

---
+++ script.js
@@ -0,0 +1,29 @@
+const carouselInner = document.querySelector('.carousel-inner');
+    let currentIndex = 0;
+
+    function nextImage() {
+        currentIndex++;
+        if (currentIndex >= carouselInner.children.length) {
+            currentIndex = 0;
+        }
+
+        carouselInner.style.transform = `translateX(-${currentIndex * 100}%`;
+    }
+
+    setInterval(nextImage, 5000); // Mudar imagem a cada 5 segundos
+function playmusic(path)
+{
+    const audioPlayer = document.getElementById('audioPlayer');
+    const audioSource = document.getElementById('audioSource');
+    var alt = path;
+    console.log(alt)
+    audioSource.src = alt
+    audioPlayer.load();
+    audioPlayer.play();
+}
+
+document.addEventListener("DOMContentLoaded",() =>{
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.load();
+    audioPlayer.play();
+})
